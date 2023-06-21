c469d62d3477d2cba77fb26e0aeb12d965946da5
Ihor Lytvynenko <igor@ert.org.ua>
2023-06-21 23:10:00 +0300

    *ci

--- ci-parse-tag.sh
+++ ci-parse-tag.sh
@@ -5,16 +5,20 @@ then
 	NAME=$(echo $CI_COMMIT_TAG | cut -db -f1)
 	NUM=$(echo $CI_COMMIT_TAG | cut -db -f2)
 	NAME=$NAME.$NUM
-	
-	if [[ "$OSTYPE" == "darwin"* ]]; then
-		NAME=${NAME}i
-	else
-		NAME=${NAME}a
-	fi
+
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    NAME=${NAME}i
+  elif [[ "$OSTYPE" == "msys" ]]; then
+    NAME=${NAME}w
+  else
+    NAME=${NAME}a
+  fi
 else
 	if [[ "$OSTYPE" == "darwin"* ]]; then
 		PLATFORM=ios
-	else
+	elif [[ "$OSTYPE" == "msys" ]]; then
+    PLATFORM=win
+  else
 		PLATFORM=droid
 	fi
 
@@ -25,3 +29,9 @@ fi
 BUILD_OPTS="--build-name=$NAME --build-number=$NUM"
 echo Build options from tag: $BUILD_OPTS
 
+rm -rf env.ps1
+if [[ "$OSTYPE" == "msys" ]]; then
+	echo "\$NAME='$NAME'" > env.ps1
+	echo "\$NUM='$NUM'" >> env.ps1
+	echo "\$BUILD_OPTS='$BUILD_OPTS'" >> env.ps1
+fi
