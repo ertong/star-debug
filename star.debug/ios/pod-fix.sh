b799f2acd3bf6fc3c3b758dcde5dc96aebade599
Ihor Lytvynenko <igor@ert.org.ua>
2026-06-08 23:34:42 +0300

    *ios

--- star.debug/ios/pod-fix.sh
+++ star.debug/ios/pod-fix.sh
@@ -1,4 +1,4 @@
 bundle update
-pod repo update
-pod update sqlite3 Firebase/CoreOnly Firebase/Crashlytics
-pod update
+#pod repo update
+#pod update sqlite3 Firebase/CoreOnly Firebase/Crashlytics
+#pod update
